3ec6bf531326f3444eff4d0dea416acc89974e7b
rubinoae <anthony@rubino.us>
2020-03-19 09:39:33 -0700

    Add files via upload

---
+++ kali_special.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt-get update
+apt-get -y install kali-linux-wireless
+apt-get -y install git
+apt-get -y install libcurl4-openssl-dev 
+apt-get -y install libssl-dev
+apt-get -y install zlib1g-dev
+apt-get -y install libpcap-dev
+git clone https://github.com/ZerBea/hcxtools.git
+git clone https://github.com/ZerBea/hcxdumptool.git
+cd hcxtools
+make
+make install
+cd ~
+cd hcxdumptool
+make
+make install
+cd ~
+apt-get -y install kali-tools-passwords
+apt-get -y install bettercap
+git clone https://github.com/hashcat/hashcat.git
+cd hashcat
+make
+make install
